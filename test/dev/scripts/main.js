65b3d8fc5b7c526a7d078377d7bcccb696625d4f
Thierry Michel <thmichel@gmail.com>
2015-05-07 10:32:44 +0200

    update test content

--- test/dev/scripts/main.js
+++ test/dev/scripts/main.js
@@ -6,12 +6,5 @@
   /*
    * Code here...
    */
-  // init overlay slide theatre
-  $('.theatre').after('<div class="theatre-overlay">');
-  // click handler
-  $('.theatre__title').on('click', function() {
-    $(this).parent('.theatre').toggleClass('is-open');
-    $('body').toggleClass('is-sticky');
-    $('.theatre-overlay').fadeToggle(500);
-  });
+  console.log($);
 }(jQuery));
